51426c3c40df8f7a196064724c850ba414ab613a
Kai Kewley <kaikewley@gmail.com>
2017-01-18 08:46:19 +0000

    Optimise imports

--- src/main/java/org/ihtsdo/otf/authoringtemplate/App.java
+++ src/main/java/org/ihtsdo/otf/authoringtemplate/App.java
@@ -1,19 +1,7 @@
 package org.ihtsdo.otf.authoringtemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.ihtsdo.otf.authoringtemplate.service.JsonStore;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.io.File;
-
-import static com.google.common.base.Predicates.not;
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 public class App extends Config {
