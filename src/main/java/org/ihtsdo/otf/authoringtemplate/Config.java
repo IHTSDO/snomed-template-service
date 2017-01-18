0608c37e4e3333113a1262de6bf394ba8c9979ca
Kai Kewley <kaikewley@gmail.com>
2017-01-18 08:46:00 +0000

    Add SSO authentication.

--- src/main/java/org/ihtsdo/otf/authoringtemplate/Config.java
+++ src/main/java/org/ihtsdo/otf/authoringtemplate/Config.java
@@ -3,10 +3,18 @@ package org.ihtsdo.otf.authoringtemplate;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.ihtsdo.otf.authoringtemplate.service.JsonStore;
+import org.ihtsdo.sso.integration.RequestHeaderAuthenticationDecorator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -39,6 +47,25 @@ public class Config {
 				.apis(RequestHandlerSelectors.any())
 				.paths(not(regex("/error")))
 				.build();
+	}
+
+	// Security
+	@Configuration
+	@EnableWebSecurity
+	@Order(1)
+	public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests()
+					.antMatchers("/swagger-ui.html",
+							"/swagger-resources/**",
+							"/v2/api-docs",
+							"/webjars/springfox-swagger-ui/**").permitAll()
+					.anyRequest().authenticated()
+					.and().httpBasic();
+			http.csrf().disable();
+			http.addFilterAfter(new RequestHeaderAuthenticationDecorator(), BasicAuthenticationFilter.class);
+		}
+
 	}
 
 }
