d0a691f80666b27595c07b59a041414221dd4575
Kai Kewley <kaikewley@gmail.com>
2017-02-16 16:04:37 +0000

    Log warning if template-store directory can not be created.

--- src/main/java/org/ihtsdo/otf/authoringtemplate/service/JsonStore.java
+++ src/main/java/org/ihtsdo/otf/authoringtemplate/service/JsonStore.java
@@ -1,14 +1,20 @@
 package org.ihtsdo.otf.authoringtemplate.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class JsonStore {
 
 	private static final String EXTENSION = ".json";
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	private static final FilenameFilter FILENAME_FILTER = new FilenameFilter() {
 		@Override
@@ -60,7 +66,9 @@ public class JsonStore {
 
 	private File getClassDir(Class<?> clazz) {
 		final File classDir = new File(storeDirectory, clazz.getSimpleName());
-		classDir.mkdirs();
+		if (!(classDir.exists() || classDir.mkdirs())) {
+			logger.warn("Failed to create directory {}", classDir.getAbsolutePath());
+		}
 		return classDir;
 	}
 
