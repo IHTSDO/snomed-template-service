b98f247021beb974f836313d2c2c3b096fdeec98
Kai Kewley <kaikewley@gmail.com>
2020-05-12 14:38:21 +0100

    MAINT-849 Verify TSV header line before proceeding.

--- src/main/java/org/ihtsdo/otf/transformationandtemplate/service/componenttransform/TSVTransformationStream.java
+++ src/main/java/org/ihtsdo/otf/transformationandtemplate/service/componenttransform/TSVTransformationStream.java
@@ -1,32 +1,44 @@
 package org.ihtsdo.otf.transformationandtemplate.service.componenttransform;
 
+import org.ihtsdo.otf.rest.exception.BadRequestException;
+import org.ihtsdo.otf.rest.exception.BusinessServiceRuntimeException;
 import org.ihtsdo.otf.transformationandtemplate.domain.TransformationRecipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
+
+import static java.lang.String.format;
 
 public class TSVTransformationStream implements TransformationStream {
 
 	private final Map<String, Object> fieldMap;
 	private final BufferedReader bufferedReader;
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private boolean readFirst;
 
-	public TSVTransformationStream(TransformationRecipe recipe, BufferedReader bufferedReader) {
+	public TSVTransformationStream(TransformationRecipe recipe, BufferedReader bufferedReader) throws IOException, BadRequestException {
 		this.fieldMap = recipe.getFieldMap();
 		this.bufferedReader = bufferedReader;
+
+		// Read and verify header line
+		String header = bufferedReader.readLine();
+		String[] columns = header.split("\\t");
+		Collection<String> fieldNames = recipe.getFieldNames();
+		if (columns.length != fieldNames.size()) {
+			throw new BadRequestException(format("First line of TSV file must contain the list of fields. " +
+					"For this transformation %s fields are expected (%s) but found %s.", fieldNames.size(), fieldNames, columns.length));
+		}
+		List<String> columnsList = Arrays.asList(columns);
+		if (fieldNames.containsAll(columnsList)) {
+			throw new BadRequestException(format("First line of TSV file must contain the list of fields. " +
+					"For this transformation the expected fields are %s (not in order) but found %s.", fieldNames, columnsList));
+		}
 	}
 
 	@Override
 	public ComponentTransformation next() throws IOException {
-		if (!readFirst) {
-			// Discard header line
-			bufferedReader.readLine();
-			readFirst = true;
-		}
 		String line = bufferedReader.readLine();
 		if (line == null) {
 			return null;
