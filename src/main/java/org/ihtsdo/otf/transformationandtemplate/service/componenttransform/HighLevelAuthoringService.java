ecb163f0227690e2124d88fd041120c6caf47edf
Quyen Ly <quocquyenit@gmail.com>
2022-02-24 09:49:13 +0700

    Revert "MAINT-1788 Support for Not Acceptable option in Bulk Import Template"
    This reverts commit 6c4b22093a83012202562db93c09a5a7e4593e42.

--- src/main/java/org/ihtsdo/otf/transformationandtemplate/service/componenttransform/HighLevelAuthoringService.java
+++ src/main/java/org/ihtsdo/otf/transformationandtemplate/service/componenttransform/HighLevelAuthoringService.java
@@ -586,7 +586,6 @@ public class HighLevelAuthoringService {
 						loadedDescription.setModuleId(descriptionUpdate.getModuleId());
 					}
 					if (descriptionUpdate.getAcceptabilityMap() != null) {
-						descriptionUpdate.getAcceptabilityMap().values().removeAll(Collections.singleton(null));
 						loadedDescription.setAcceptabilityMap(descriptionUpdate.getAcceptabilityMap());
 					}
 					if (!descriptionUpdate.isActive()) {
