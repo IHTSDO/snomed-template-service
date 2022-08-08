c38dc71bb9f08f96b8675515340829e77c47f232
Elena Ilyukhina <eil@ihtsdo.org>
2022-08-08 17:32:53 +0100

    Revert "Added logging to VersionController (will be reverted)"
    This reverts commit 72f2f60a812c1af4580743728085e1b0825b9b5c.

--- src/main/java/org/ihtsdo/otf/transformationandtemplate/rest/VersionController.java
+++ src/main/java/org/ihtsdo/otf/transformationandtemplate/rest/VersionController.java
@@ -2,10 +2,7 @@ package org.ihtsdo.otf.transformationandtemplate.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,20 +17,10 @@ public class VersionController {
 	@Autowired
 	BuildProperties buildProperties;
 
-	@Value("${spring.security.user.name}")
-	private String username;
-
-	@Value("${spring.security.user.password}")
-	private String password;
-
-	private final static Logger LOGGER = LoggerFactory.getLogger(VersionController.class);
-
 	@ApiOperation("Software build version and timestamp.")
 	@RequestMapping(value = "/version", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public BuildVersion getBuildInformation() {
-		LOGGER.info("Configured 'spring.security.user.name': {}", username);
-		LOGGER.info("Configured 'spring.security.user.password': {}", password);
 		return new BuildVersion(buildProperties.getVersion(), buildProperties.getTime().toString());
 	}
 
