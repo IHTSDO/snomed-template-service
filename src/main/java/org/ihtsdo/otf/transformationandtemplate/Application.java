2a08d5980bdb48a10b0756e852c9aff6bc1386f3
CoderMChu <mch@ihtsdo.org>
2023-11-10 11:03:23 +0000

    PIP-381 Replace deprecated workaround for encoded slash to work with tomcat 10

--- src/main/java/org/ihtsdo/otf/transformationandtemplate/Application.java
+++ src/main/java/org/ihtsdo/otf/transformationandtemplate/Application.java
@@ -1,7 +1,10 @@
 package org.ihtsdo.otf.transformationandtemplate;
 
+import org.apache.tomcat.util.buf.EncodedSolidusHandling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
@@ -9,7 +12,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
 
 	public static void main(String[] args) {
-		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
 		SpringApplication.run(Application.class, args);
 	}
+	@Bean
+	public TomcatConnectorCustomizer connectorCustomizer() {
+		// Swagger encodes the slash in branch paths
+		return connector -> connector.setEncodedSolidusHandling(EncodedSolidusHandling.DECODE.getValue());
+	}
+
 }
